15023ffadad0cd5b11d764b144089011485db520
franců <francupatrik@seznam.cz>
2025-07-09 22:29:25 +0200

    - Update feature 1.1, 1.2 - Styling, linkage

--- SuperEvents/settings.py
+++ SuperEvents/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'viewer' / 'static',
+]
 
 # Media files (User uploaded files)
 MEDIA_URL = '/media/'
--- SuperEvents/urls.py
+++ SuperEvents/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from viewer.views import home, EventsListView, EventDetailView
 
@@ -31,3 +32,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
