da84a04219f732000069d32971b829c7805ce8f1
franců <francupatrik@seznam.cz>
2025-07-27 12:31:56 +0200

    FIX Errors and warnings on http://127.0.0.1:8000/events/ FIX Language settings.py

--- SuperEvents/settings.py
+++ SuperEvents/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -114,13 +115,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'cs-cs'
+LANGUAGE_CODE = 'cs'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LANGUAGES = [
+    ('cs', 'Czech'),
+    ('en', 'English'),
+]
 
 
 # Static files (CSS, JavaScript, Images)
